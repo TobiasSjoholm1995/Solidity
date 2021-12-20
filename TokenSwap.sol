3a1f586da2cfc0c66d71d6ea2f83036ffba64c90
TobiasSjoholm1995 <43572826+TobiasSjoholm1995@users.noreply.github.com>
2021-12-20 09:51:31 +0100

    Update TokenSwap.sol

--- TokenSwap.sol
+++ TokenSwap.sol
@@ -6,9 +6,10 @@ import "./IERC20.sol";
 
 // How to use the TokenSwap:
 // 1. Anyone can create the contract and configure the tokens and amounts
-// 2. Both traders will get the contract address and can veify that the token address and amount is correct configured
-// 3. Both traders set their own allowence for their token
-// 4. Any of the traders can call swap function, to execute the trade
+// 2. Send the contract address to both traders
+// 3. Both traders verify that the token address and amount is correctly configured
+// 4. Both traders set their own allowence for their token
+// 5. Any of the traders can call swap function, to execute the trade
 
 
 contract TokenSwap {
@@ -19,32 +20,30 @@ contract TokenSwap {
 
     address private immutable account1;
     IERC20 public immutable token1;
-    uint256 public immutable expectedAmount1;
+    uint256 public immutable amount1;
 
     address private immutable account2;
     IERC20 public immutable token2;
-    uint256 public immutable expectedAmount2;
+    uint256 public immutable amount2;
 
     constructor(
-        address account1_, address token1_, uint256 expectedAmunt1_, 
-        address account2_, address token2_, uint256 expectedAmunt2_)
+        address account1_, address token1_, uint256 amount1_, 
+        address account2_, address token2_, uint256 amount2_)
     {
-        account1        = account1_;
-        token1          = IERC20(token1_);
-        expectedAmount1 = expectedAmunt1_;
+        account1 = account1_;
+        token1   = IERC20(token1_);
+        amount1  = amount1_;
 
-        account2        = account2_;
-        token2          = IERC20(token2_);
-        expectedAmount2 = expectedAmunt2_;
+        account2 = account2_;
+        token2   = IERC20(token2_);
+        amount2  = amount2_;
     }
 
 
-    function swap(uint256 amount1, uint256 amount2) public {
+    function swap() public {
         require(msg.sender == account1 || msg.sender == account2, "Not authorized.");
-        require(expectedAmount1 == amount1, "Invalid amount for token1");
-        require(expectedAmount2 == amount2, "Invalid amount for token2");
         require(token1.allowance(account1, address(this)) >= amount1, "Too low allowence for token1.");
-        require(token2.allowance(account2, address(this)) >= amount1, "Too low allowence for token2.");
+        require(token2.allowance(account2, address(this)) >= amount2, "Too low allowence for token2.");
 
         bool success1 = token1.transferFrom(account1, account2, amount1);
         bool success2 = token2.transferFrom(account2, account1, amount2);
