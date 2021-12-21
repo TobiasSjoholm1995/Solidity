3f541d393a6d8b235fc936defb06e26d6c996d83
TobiasSjoholm1995 <43572826+TobiasSjoholm1995@users.noreply.github.com>
2021-12-21 14:24:52 +0100

    Update LiquidityPool.sol

--- LiquidityPool.sol
+++ LiquidityPool.sol
@@ -4,117 +4,139 @@ pragma solidity ^0.8.0;
 import "./IERC20.sol";
 
 contract LiquidityPool {
-    IERC20 public immutable _token1;
-    IERC20 public immutable _token2;
+    IERC20 public immutable token1;
+    IERC20 public immutable token2;
 
-    uint256 public _balance1;
-    uint256 public _balance2;
+    uint256 public balance1;
+    uint256 public balance2;
+
+    uint256 private _depositProduct;
 
     mapping(address => uint256) private _provider;
 
 
-    constructor(address token1, address token2) {
-        require(token1 != token2, "Invalid arguments, token addresses can't be equal.");
+    constructor(address token1_, address token2_) {
+        require(token1_ != token2_, "Invalid arguments, token addresses can't be equal.");
         
-        _token1 = IERC20(token1);
-        _token2 = IERC20(token2);
+        token1 = IERC20(token1_);
+        token2 = IERC20(token2_);
     }
 
 
     function swapToken1ForToken2(uint256 amount) external {
         uint256 returnAmount = getReturnAmount(amount, true);
 
-        require(_token1.allowance(msg.sender, address(this)) >= amount, "Liquidity Pool: Allowance too low.");
-        require(_balance2 >= returnAmount, "Liquidity Pool: Not enough tokens in the pool.");
+        require(token1.allowance(msg.sender, address(this)) >= amount, "Liquidity Pool: Allowance too low.");
+        require(balance2 >= returnAmount, "Liquidity Pool: Not enough tokens in the pool.");
 
-        bool success1 = _token1.transferFrom(msg.sender, address(this), amount);
-        bool success2 = _token2.transfer(msg.sender, returnAmount);
+        bool success1 = token1.transferFrom(msg.sender, address(this), amount);
+        bool success2 = token2.transfer(msg.sender, returnAmount);
 
         require(success1 && success2, "Liquidity Pool: Transfer of tokens failed.");
 
-        _balance1 += amount;
-        _balance2 -= returnAmount;
+        balance1 += amount;
+        balance2 -= returnAmount;
     }
 
     function swapToken2ForToken1(uint256 amount) external {
         uint256 returnAmount = getReturnAmount(amount, false);
 
-        require(_token2.allowance(msg.sender, address(this)) >= amount, "Liquidity Pool: Allowance too low.");
-        require(_balance1 >= returnAmount, "Liquidity Pool: Not enough tokens in the pool.");
+        require(token2.allowance(msg.sender, address(this)) >= amount, "Liquidity Pool: Allowance too low.");
+        require(balance1 >= returnAmount, "Liquidity Pool: Not enough tokens in the pool.");
 
-        bool success1 = _token2.transferFrom(msg.sender, address(this), amount);
-        bool success2 = _token1.transfer(msg.sender, returnAmount);
+        bool success1 = token2.transferFrom(msg.sender, address(this), amount);
+        bool success2 = token1.transfer(msg.sender, returnAmount);
 
         require(success1 && success2, "Liquidity Pool: Transfer of tokens failed.");
 
-        _balance1 -= returnAmount;
-        _balance2 += amount;
+        balance1 -= returnAmount;
+        balance2 += amount;
     }
 
 
-    // note that you tokens are atomic
-    // so transfering few tokens is bad as it rounds off towards 0
+    // 0.5% swap fee with minumum of 1 token (caused by round off)
+    // so transfering few tokens is bad 
     function getReturnAmount(uint256 amount, bool token1ForToken2) public view returns (uint256) {
-        require(_balance1 != 0 && _balance2 != 0, "Liquidity Pool: There is no available tokens.");
+        require(balance1 != 0 && balance2 != 0, "Liquidity Pool: There is no available tokens.");
         require(amount != 0, "Liquidity Pool: Invalid argument, amount is zero.");
 
         // using the Constant Product Formula
-        uint256 product = _balance1 * _balance2; 
-
+        uint256 product = balance1 * balance2; 
+        uint256 returnAmount = 0;
+        
         if (token1ForToken2) 
-            return _balance2 - (product / (_balance1 + amount) + 1);
+            returnAmount =  balance2 - (product / (balance1 + amount) + 1);
         else
-            return _balance1 - (product / (_balance2 + amount) + 1);
+            returnAmount = balance1 - (product / (balance2 + amount) + 1);
+
+        uint256 fee = returnAmount * 5 / 1000;
+
+        return returnAmount - fee;
     }
 
     
     function _percent(uint256 numerator, uint256 denominator) private pure returns(uint256 quotient) {
         uint _numerator = numerator * 10 ** 2;
-        uint _quotient  =  _numerator / denominator;
+        uint _quotient  = _numerator / denominator;
 
         return _quotient;
     }
 
 
     function deposit(uint256 amount) external {
-        require(_token1.allowance(msg.sender, address(this)) >= amount, "Liquidity Pool: Token1 allowance too low.");
-        require(_token2.allowance(msg.sender, address(this)) >= amount, "Liquidity Pool: Token2 allowance too low.");
+        require(token1.allowance(msg.sender, address(this)) >= amount, "Liquidity Pool: Token1 allowance too low.");
+        require(token2.allowance(msg.sender, address(this)) >= amount, "Liquidity Pool: Token2 allowance too low.");
 
-        bool success1 = _token1.transferFrom(msg.sender, address(this), amount);
-        bool success2 = _token2.transferFrom(msg.sender, address(this), amount);
+        bool success1 = token1.transferFrom(msg.sender, address(this), amount);
+        bool success2 = token2.transferFrom(msg.sender, address(this), amount);
 
         require(success1 && success2, "Liquidity Pool: Transfer of tokens failed.");
 
-        _balance1 += amount;
-        _balance2 += amount;
+        balance1 += amount;
+        balance2 += amount;
         _provider[msg.sender] += amount;
-
-        _overflowCheck();
+        _depositProduct = balance1 * balance2;
     }
 
     function withdraw(uint256 amount) external {
         require(_provider[msg.sender] >= amount, "Liquidity Pool: Withdraw amount exceeds provide amount.");
-        require(_balance1 >= amount, "Liquidity Pool: There is not enough token1 balance to withdraw right now.");
-        require(_balance2 >= amount, "Liquidity Pool: There is not enough token2 balance to withdraw right now.");
+        require(balance1 >= amount, "Liquidity Pool: There is not enough token1 balance to withdraw right now.");
+        require(balance2 >= amount, "Liquidity Pool: There is not enough token2 balance to withdraw right now.");
 
-        bool success1 = _token1.transfer(msg.sender, amount);
-        bool success2 = _token2.transfer(msg.sender, amount);
+        bool success1 = token1.transfer(msg.sender, amount);
+        bool success2 = token2.transfer(msg.sender, amount);
 
         require(success1 && success2, "Liquidity Pool: Transfer of tokens failed.");
 
-        _balance1 -= amount;
-        _balance2 -= amount;
+        balance1 -= amount;
+        balance2 -= amount;
         _provider[msg.sender] -= amount;
+        _giveReward(amount);
     }
 
+    function _giveReward(uint256 withdrawAmount_) private{
+        require(balance1 != 0 && balance2 != 0, "Liquidity Pool: There is no available tokens.");
+        
+        uint256 currentProduct     = balance1 * balance2;
+        uint256 percentTotalReward = (currentProduct * 100 / _depositProduct) - 100;
+        uint256 percentUserReward  = (withdrawAmount_ + withdrawAmount_) * 100 / (balance1 + balance2);
+
+        if (balance1 >= balance2) {
+            uint256 rewardAmount1 = (percentTotalReward * (percentUserReward * balance1)) / 10000;
+            token1.transfer(msg.sender, rewardAmount1);
+            balance1 -= rewardAmount1;
+        }
+        else {
+            uint256 rewardAmount2 = (percentTotalReward * (percentUserReward * balance2)) / 10000;
+            token2.transfer(msg.sender, rewardAmount2);
+            balance2 -= rewardAmount2;
+        }
+
+        _depositProduct = balance1 * balance2;
+    }
     
     function withdrawAmount(address account) external view returns (uint256) {
         return _provider[account];
     }
 
-    function _overflowCheck() private view {
-        unchecked {
-            require (_balance1 * _balance2 / _balance2 == _balance1, "Liquidity Pool: The deposit casued the pool to overflow.");
-        }
-    }
 }
