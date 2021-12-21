786cdbac4c22f27ad0f9276dc3454b0524acb4ea
TobiasSjoholm1995 <43572826+TobiasSjoholm1995@users.noreply.github.com>
2021-12-22 00:36:55 +0100

    Update and rename EnterGuard.sol to Guard.sol

---
+++ Guard.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+abstract contract Guard {
+
+    // uint256 saves gas comapared to bool
+    uint256 private constant _allowed = 1;
+    uint256 private constant _not_allowed = 2;
+
+    uint256 private _status;
+
+    constructor() {
+        _status = _allowed;
+    }
+
+    modifier guard() {
+        require(_status == _allowed, "EnterGuard: Not allowed to enter.");
+        _status = _not_allowed;
+        _;
+        _status = _allowed;
+    }
+}
